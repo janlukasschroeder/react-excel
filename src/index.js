63c7385cb8b108c24555f6a74dca7f719d90e759
janlukasschroeder <janlukasschroeder@protonmail.com>
2018-12-19 17:03:52 +1100

    Release

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import TableWrapper from "./components/TableWrapper";
 import { Provider } from "react-redux";
 import configStore from "./store";
@@ -7,11 +6,10 @@ import configStore from "./store";
 import "./styles.css";
 
 const store = configStore();
-const rootElement = document.getElementById("root");
-
-ReactDOM.render(
+const render = () => (
   <Provider store={store}>
     <TableWrapper />
-  </Provider>,
-  rootElement
+  </Provider>
 );
+
+export default render;
